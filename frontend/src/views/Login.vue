bd1a0160f1abca8d2f5766d1d391e3017e09bafe
felixqian <felix.qian@ximalaya.com>
2021-07-22 14:40:49 +0800

    add frontend

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+				<i class="el-icon-s-custom header"></i>
+				<span class="header"> Login</span>
+			</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+	@import "../assets/css/container.css";
+</style>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <i class="el-icon-s-custom header"></i>
+        <span class="header">Register
+        </span>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<style>
+	@import "../assets/css/container.css";
+</style>
